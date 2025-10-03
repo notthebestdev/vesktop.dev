69660a938f7f33ef53efed060f8a4486cc93f235
Vendicated <vendicated@riseup.net>
2025-10-03 02:41:53 +0200

    update icon, domain & deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://vesktop.vencord.dev",
+    site: "https://vesktop.dev",
 
     integrations: [
         starlight({
